46958bb294f6020f7009a3f03dada6066c5f1ca0
Anurag Ghosh <anuragghosh66@gmail.com>
2022-11-27 12:30:19 +0530

    Second Commit

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Password Generator v2 is the second version of my first ever Web Development Project.
 This web app is madw with React!
 
-![pass](https://user-images.githubusercontent.com/82631826/204021995-04bf1a81-e9d2-41d0-98f7-4a0f68a2c543.png)
+![pass](https://user-images.githubusercontent.com/82631826/204123026-002dc45d-100c-49cc-80bf-3edf4016fbce.png)
 
 It firstly takes the length of the password as input, And with few *secrets ,
 It generates very promising passwords for you to use!
@@ -11,4 +11,4 @@ And the most unique part? It's UI! Go experience it!
 
 ### Tools used
 1. Figma (For Design and Prototyping)
-2. HTML, Sass, Typescript, ReactJS, After Effects.
+2. HTML, Sass, Typescript, ReactJS, After Effects.# Naughty-Passwords
---
+++ src/main.tsx
@@ -0,0 +1,182 @@
+import React from 'react';
+import { reverse, showAlert } from './Script/operations';
+import copy from 'copy-to-clipboard';
+import crtVid from './resources/crt.mp4';
+import copyButton from './resources/Copy.svg';
+import regenerateButton from './resources/Refresh.svg'
+import './Style/App.css';
+
+class Main extends React.Component<{}, { password: string, strength: string }>{
+  
+  constructor(props:any){
+    super(props);
+    this.state = {
+      password : "(^_^)",
+      strength : "Strength",
+    }
+  }
+
+  render(){
+    //Variables & Constants
+    var uncheckedIndex : number = 0;
+
+    //Check Strength
+    const checkStrength = (password:string) =>{
+      var len = password.length;
+
+      if(len<12){
+        this.setState({strength:"Usable"});
+        return;
+      }
+      if(len<18){
+        this.setState({strength:"Ideal"});
+        return;
+      }
+      if(len<25){
+        this.setState({strength:"Strong"});
+        return;
+      }
+    }
+
+    //Password Generator
+    const setPassword = (e?:any) =>{
+      var trigger=4;
+      const checkBox = document.querySelectorAll('.CharCheckBox');
+
+      for(let i=0;i<checkBox.length;i++){
+        let x : HTMLInputElement = checkBox[i] as HTMLInputElement;  
+        if (x.checked !== true){
+          trigger-=1;
+          uncheckedIndex=i;
+        }  
+      }
+
+      if(trigger <= 1){
+        let x : HTMLInputElement = checkBox[uncheckedIndex] as HTMLInputElement;  
+        x.checked = true;
+        setPassword();
+      }
+
+      var lenInputVal = document.getElementById("lenInput") as HTMLInputElement;
+      let length : number = Number(lenInputVal.value); //Length of password
+
+      if (length > 25 || length < 8){
+        showAlert("Firstly enter a valid length between 8 and 25.",1850,"error");
+        return;
+      }
+
+      var generated:string = "";
+      var options:string = "";
+
+      checkBox.forEach(checkBox =>{
+        let x : HTMLInputElement = checkBox as HTMLInputElement;  
+        if (x.checked === true){
+          let chars:string = x.value;
+          options+=chars;
+        }
+      }) 
+
+      let n = options.length;
+      for(let i=0;i<length;i++){
+        let randIndex:number = Math.floor(Math.random() * n);
+        generated+=options[randIndex];
+      }
+      
+      trigger=0;
+      this.setState({password:`${reverse(generated)}`});
+      checkStrength(generated);
+    }
+
+    //Copy to clipboard
+    const copyToClipboard = () =>{
+      if (String(this.state.password).length < 8 ){
+        showAlert("Generate a password first!",1500,"error");
+        return;
+      }
+      copy(this.state.password);
+      showAlert("Password copied successfully!",1000,"success");
+      // navigator.clipboard.writeText(String(this.state.password));
+    }
+
+    //Slider & Input Control
+    const lenInputChange = (e:React.FormEvent) => {
+      const lenInputVal = document.getElementById("lenInput") as HTMLInputElement;
+      const lenSliderVal = document.getElementById("lenSlider") as HTMLInputElement;
+      lenSliderVal.value = lenInputVal.value;
+      setPassword();
+    }
+    
+    const sliderInputChange = (e:React.FormEvent) => {
+      const lenInputVal = document.getElementById("lenInput") as HTMLInputElement;
+      const lenSliderVal = document.getElementById("lenSlider") as HTMLInputElement;
+      lenInputVal.value = lenSliderVal.value;
+      setPassword();
+    }
+
+    return (
+      <>
+        <video src={crtVid} autoPlay muted loop id="myVideo"/>
+        <div className="alertHolder">
+          <div>
+            <p id='alertText'/>
+          </div>
+        </div>
+        <div className="Main">
+          <div className="container">
+            <header>
+              <h1>NAUGHTY PASSWORDS</h1>
+            </header>
+  
+            <div className="controls">
+  
+              <div className="control1">
+                <div className="length">
+                  <h3>Length</h3>
+                  <input type="number" id="lenInput" min="8" max="25" onChange={lenInputChange}/>
+                </div>
+                <input type="range" id="lenSlider" min="8" max="25" onInput={sliderInputChange} />
+              </div>
+  
+              <div className="control2">
+                <form action="/action_page.php">
+                  <label htmlFor="Numbers">Numbers</label>
+                  <input type="checkbox" className='CharCheckBox' id="numbers" name="Numbers" value="0123456789" defaultChecked={true} onChange={setPassword}/>
+                </form>
+                <form action="">
+                  <label htmlFor="Uppercase">Uppercase</label>
+                  <input type="checkbox" className='CharCheckBox' id="uppercase" name="Uppercase" value="ABCDEFGHIJKLMNOPQRSTUVWXYZ" defaultChecked={true} onChange={setPassword}/>
+                </form>
+              </div>
+              
+              <div className="control3">
+                <form action="/action_page.php">
+                  <label htmlFor="Symbols">Symbols</label>
+                  <input type="checkbox" className='CharCheckBox' id="symbols" name="Symbols" value={"~`!@#$%^&*()_-+={[}]|:;'<,>.?"} defaultChecked={true} onChange={setPassword}/>
+                </form>
+                <form action="">
+                  <label htmlFor="Lowercase">Lowercase</label>
+                  <input type="checkbox" className='CharCheckBox' id="lowercase" name="Lowercase" value="abcdefghijklmnopqrstuvwxyz" defaultChecked={true} onChange={setPassword}/>
+                </form>
+              </div>
+            </div>
+            <div id='strength' className={this.state.strength}>
+              <h3>{this.state.strength}</h3>
+            </div>
+            <div className="result">
+              <div className="passwordHolder">
+                <h3 id="password">{this.state.password}</h3>
+              </div>
+              <div className="buttonHolder">
+                <button onClick={copyToClipboard}><img src={copyButton} alt="Copy" title='Copy to Clipboard!' /></button>
+                <button onClick={setPassword}><img src={regenerateButton} alt="Copy" title='Generate new Password!' /></button>
+              </div>
+            </div>
+            <p>Developed with &#x2764; by <span style={{color:"#88e9a5"}}>Anurag Ghosh</span></p>
+          </div>
+        </div>
+      </>
+    );
+  }
+}
+
+export default Main;
